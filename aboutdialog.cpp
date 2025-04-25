7bfb8d8b8fb93be09c7b5ceb08aa2bea12295ec7
Shinjo Park <peremen@gmail.com>
2025-04-26 00:57:01 +0200

    aboutdialog: update copyright, move to markdown

--- aboutdialog.cpp
+++ aboutdialog.cpp
@@ -22,6 +22,7 @@ AboutDialog::AboutDialog(QWidget *parent) :
     ui(new Ui::AboutDialog)
 {
     ui->setupUi(this);
+    ui->textBrowser->setMarkdown("3GPP Decoder v1.0\n\nAuthor: [Prashant Panigrahi](prashant@3glteinfo.com), Shinjo Park\n\nWebsite: [https://github.com/peremen/3GPPDecoder](https://github.com/peremen/3GPPDecoder)\n\nCopyright (C) 2015 Prashant Panigrahi\n\nCopyright (C) 2015-2025 Shinjo Park\n\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\nThis program is distributed in the hope that it will be useful, but WITHOUT ANY  WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A  PARTICULAR PURPOSE. See the GNU General Public License for more details.\n\nYou should have received a copy of the GNU General Public License along with this program. If not, see <http://www.gnu.org/licenses>");
 }
 
 AboutDialog::~AboutDialog()
